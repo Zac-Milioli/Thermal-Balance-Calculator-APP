87f5738cd2ce498869fa1ed4c7e25ecf787fbbe3
Zac-Milioli <zacmilioli@gmail.com>
2024-06-26 14:32:24 -0300

    tratamento de erro para mensal ou anual

--- pages/plotter.py
+++ pages/plotter.py
@@ -28,38 +28,40 @@ if csv_file:
 
     range_opt = st.selectbox(label="Coverage", options=["annual", "monthly"], index=0, placeholder="annual")
 
-    with st.form("form_plotter", border=False):
-        zones_in_df = dataframe["zone"].unique()
-        zones_multiselect = st.multiselect(label="Zones", options=zones_in_df, placeholder="All Zones")
-        type_opt = st.selectbox(label="Type", options=["convection", "surface"], index=0, placeholder="convection")
-        
-        col1, col2 = st.columns(2)
+    try:    
+        with st.form("form_plotter", border=False):
+            zones_in_df = dataframe["zone"].unique()
+            zones_multiselect = st.multiselect(label="Zones", options=zones_in_df, placeholder="All Zones")
+            type_opt = st.selectbox(label="Type", options=["convection", "surface"], index=0, placeholder="convection")
+            
+            col1, col2 = st.columns(2)
 
-        use_tight = col1.checkbox(label="Tight layout")
-        cbar_loc = col2.radio(label="Color bar location", options=["bottom", "right", "top", "left"], index=0, horizontal=True)
-        months_selected = False
+            use_tight = col1.checkbox(label="Tight layout")
+            cbar_loc = col2.radio(label="Color bar location", options=["bottom", "right", "top", "left"], index=0, horizontal=True)
+            months_selected = False
 
-        if range_opt == 'monthly':
-            months_on_df = dataframe['month'].unique()
-            months_selected = st.multiselect(label="Months", options=months_on_df, placeholder="All year")
-        
-        st.title("")
-        col1, col2, col3 = st.columns(3)
-        if col2.form_submit_button(label='Create Heatmap', use_container_width=True):
-            zones_opt = 0 if not zones_multiselect else zones_multiselect
-            months_opt = 0 if not months_selected else months_selected
-            filename = csv_file.name.replace(".csv", "")
-            filename = f"{filename}_{datetime.now().strftime('%H-%M-%S_%d-%m')}"
-            notify_heatmap = st.container()
-            try:
-                new_heatmap = HeatMap(df=dataframe, target_type=type_opt, zones=zones_opt, months=months_opt, tight=use_tight, cbar_orientation=cbar_loc, filename=filename)
-                if range_opt == 'annual':
-                    new_heatmap.annual()
-                elif range_opt == 'monthly':
-                    new_heatmap.monthly()
-            except Exception as e:
-                notify_heatmap.error(f'The following error occured while processing the files: {e}', icon='⚠️')
-
+            if range_opt == 'monthly':
+                months_on_df = dataframe['month'].unique()
+                months_selected = st.multiselect(label="Months", options=months_on_df, placeholder="All year")
+            
+            st.title("")
+            col1, col2, col3 = st.columns(3)
+            if col2.form_submit_button(label='Create Heatmap', use_container_width=True):
+                zones_opt = 0 if not zones_multiselect else zones_multiselect
+                months_opt = 0 if not months_selected else months_selected
+                filename = csv_file.name.replace(".csv", "")
+                filename = f"{filename}_{datetime.now().strftime('%H-%M-%S_%d-%m')}"
+                notify_heatmap = st.container()
+                try:
+                    new_heatmap = HeatMap(df=dataframe, target_type=type_opt, zones=zones_opt, months=months_opt, tight=use_tight, cbar_orientation=cbar_loc, filename=filename)
+                    if range_opt == 'annual':
+                        new_heatmap.annual()
+                    elif range_opt == 'monthly':
+                        new_heatmap.monthly()
+                except Exception as e:
+                    notify_heatmap.error(f'The following error occured while processing the files: {e}', icon='⚠️')
+    except Exception as e:
+        notify_file.error(f'ERROR: No month column detected in dataframe. Change the Coverage to match your dataframe type.', icon='⚠️')
 
 globed = glob("output/*.png")
 if len(globed) != 0:
